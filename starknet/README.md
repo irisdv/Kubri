6eaca9c1add50d4b075e35b49d939b5698286c6f
Iris <iris.devillars@gmail.com>
2021-12-13 16:05:36 +0100

    Add starknet folder to repo

---
+++ starknet/README.md
@@ -0,0 +1,14 @@
+# Starknet bridge
+
+run npm install 
+
+Testing:
+
+For pytest: 
+- run > pytest contracts_tests/test_bridged721.py 
+
+For starknet-hardhat plugin: 
+- You need to have Docker installed and running
+- Compiling : > npx hardhat starknet-compile
+- Running tests : > npx hardhat test 
+
---
+++ starknet/hardhat.config.ts
@@ -0,0 +1,19 @@
+import "@shardlabs/starknet-hardhat-plugin";
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  cairo: {
+    version: "0.6.2"
+  },
+  networks: {
+    devnet: {
+      url: "http://localhost:5000"
+    }
+  },
+  mocha: {
+    // starknetNetwork: "devnet"
+    starknetNetwork: "alpha"
+  }
+};
